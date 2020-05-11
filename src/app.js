1f119484e93dbc33ecb992524280610435570410
Rajat Bista <rajat.bista@gmail.com>
2020-05-11 20:31:08 +0545

    Setup app for herolu

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utlis/geocode')
 const forecast = require('./utlis/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -64,14 +65,6 @@ app.get('/weather', (req, res) => {
           })
       })
     })
-
-
-    //     res.send({
-    //       forecast: 'It is sunny',
-    //       location: 'Kathmandu',
-    //       address: req.query.address
-    // })
-
 })
 
 app.get('/products', (req, res) => {
@@ -103,6 +96,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port.' + port)
 })
